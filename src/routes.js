9a706fdd44ad1786f5e31f1a5650f96726d18b3e
Juliano Sirtori <julianosirtori@gmail.com>
2019-10-18 01:17:44 -0300

    Add eslint, prettier and editorconfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import {Router}  from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res)=>{
-  res.json({message: 'Teste'});
+routes.get('/teste', (req, res) => {
+  res.json({ message: 'Teste' });
 });
 
 export default routes;
