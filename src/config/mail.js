cb25244def4714844d0d8feffeff3531f17d7e93
Juliano Sirtori <julianosirtori@gmail.com>
2019-10-21 06:43:07 -0300

    Configured Mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1c6dd1b142fed7',
+    pass: 'f447792f6854e6',
+  },
+  default: {
+    from: 'Equipe Gympoint <noreplay@gympoint.com>',
+  },
+};
